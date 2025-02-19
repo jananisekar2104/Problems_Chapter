84dc6c5da06b4834cffe2eba357f7d054ee1be45
jananisekar2104 <janusekar2104@gmail.com>
2025-02-19 19:16:56 +0530

    Create prob1.java

---
+++ prob1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class prob1{
+    public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter a number:");
+    int num = scan.nextInt();
+    scan.nextLine();
+    System.out.println("Enter a word:");
+    String name =scan.nextLine();
+    System.out.println("Enter a character :");
+    char character = scan.next().charAt(0);
+    System.out.println(num);
+    System.out.println(name);
+    System.out.println(character);
+
+
+
+}}
