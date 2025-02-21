ba2c4331c7fca059b32ec2d0a381bf4824bc39bf
jananisekar2104 <janusekar2104@gmail.com>
2025-02-21 21:34:20 +0530

    Update Prob3.java

--- Prob3.java
+++ Prob3.java
@@ -1,3 +1,15 @@
+/*
+ * Given mark of student, Print the Grade
+Grade A if mark is greater than or equal to 90
+Grade B if mark is greater than or equal to 80
+Grade C if mark if greater than or equal to 60
+Grade D if mark if greaer than or equal to 35
+Fail if mark is lesser than 35
+Input: 95
+Expected Output:
+Grade A
+Explanation: Here 95 is greater than or equal to 90 so its Grade A
+ */
 import java.util.Scanner;
 class Prob3 {
     public static void main(String[]args){
