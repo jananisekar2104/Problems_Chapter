efb4116fbedc29afbc91786c5318be1fc9567eb8
jananisekar2104 <janusekar2104@gmail.com>
2025-02-21 21:32:44 +0530

    Update Prob2.java

--- Prob2.java
+++ Prob2.java
@@ -1,3 +1,12 @@
+/*
+Prob 2: Write a program to check whether a triangle can be formed with the given values for the angles.
+If sum of angles is equal to 180, then triangle can be formed, else it can't be formed.
+Input: 45 45 45
+Expected Output: 
+Triangle cannot be formed
+Explanation -> We are getting 3 inputs, that is three angles of triangle, 
+but here the sum of three angles that is 45+45+45 is not equal to 180 so Triangle cannot be formed is the output.
+*/
 import java.util.Scanner;
 class Prob2{
     public static void main(String[]args){
