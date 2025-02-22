e3ad80965caab9f27406f7fa26845ee37555c669
jananisekar2104 <janusekar2104@gmail.com>
2025-02-22 10:49:07 +0530

    Update Prob1.java

--- Prob1.java
+++ Prob1.java
@@ -1,19 +1,11 @@
 /*
 Prob 1 : Write a program that takes an integer, then a string, then a char from the user and prints them in the screen.
-
-
 Input:  2 Name y
-
 Expected Output:
-
 2
-
 Name
-
 y
-
 */
-
 import java.util.Scanner;
 class Prob1{
     public static void main(String[] args){
