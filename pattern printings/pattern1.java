f9117c95f8626868bd1893f2f0e723751a803cc0
jananisekar2104 <janusekar2104@gmail.com>
2025-02-28 17:13:04 +0530

    Add files via upload

---
+++ pattern printings/pattern1.java
@@ -0,0 +1,20 @@
+/*    N=5
+* * * * *  
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+ 
+ */
+public class pattern1 {
+     public static void main(String[]args){
+        int N = 5;
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=N;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println(" ");
+
+        }
+     }
+}
