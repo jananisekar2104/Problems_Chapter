df184726a0379458cd9073f9350040bd3abe6424
jananisekar2104 <janusekar2104@gmail.com>
2025-03-01 20:44:56 +0530

    Create pattern11.java

---
+++ pattern printings/pattern11.java
@@ -0,0 +1,24 @@
+/*                   n=5
+
+                        *********  
+                         *******
+                          *****
+                           ***
+                            *
+*/
+
+public class pattern8 {
+public static void main(String[] args) {
+    int n =5;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i-1;j++){
+            System.out.print(" ");
+        }
+        for(int j=1;j<=(2*n)-(2*i-1);j++){
+            System.out.print("*");
+        }
+        System.out.println("  ");
+        }
+    }
+}
+    
