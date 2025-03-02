3d117c231fe2219f69833e1e8e3a153036a5b17c
jananisekar2104 <janusekar2104@gmail.com>
2025-03-02 19:25:52 +0530

    Create Prob11.java

---
+++ Prob11.java
@@ -0,0 +1,44 @@
+/*
+Write a Program that takes input n and prints the below pattern and upload the code to your github repo.
+
+
+                                      
+                                      Sample 1:
+                                      
+                                      Input:
+                                      
+                                      n = 8
+                                      
+                                      Expected output:
+                                      
+                                      1 
+                                      
+                                      2 2 
+                                      
+                                      3 3 3 
+                                      
+                                      4 4 4 4 
+                                      
+                                      5 5 5 5 5 
+                                      
+                                      6 6 6 6 6 6 
+                                      
+                                      7 7 7 7 7 7 7 
+                                      
+                                      8 8 8 8 8 8 8 8 
+                                      
+*/
+
+
+class pattern11 {
+    public static void main(String[]args){
+        int n =8;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+    
+}
